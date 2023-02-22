050920efd9b8dcc60ecb94567560060afcc6f483
Orcomhiela <18257956+leabouffaut@users.noreply.github.com>
2023-02-22 14:49:55 -0500

    changed requirements and included them in setup

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup
 
 from das4whales import __version__
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='das4whales',
     version=__version__,
-    description='Machine Learning for Bioacoustics',
+    description='Distributed acoustic sensing analysis tools for Bioacoustics',
+    long_description=long_description,
     url='https://github.com/leabouffaut/DAS4Whales',
     author='Léa Bouffaut',
     author_email='lea.bouffaut@cornell.edu',
     license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
-
     py_modules=['das4whales'],
+    install_requires=['requirements'],
 )
