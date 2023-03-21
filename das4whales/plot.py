ec45ae0e1d7903ad1e49cf57377e4199b4afbf0a
Orcomhiela <18257956+leabouffaut@users.noreply.github.com>
2023-03-21 11:52:55 -0400

    update notebook. test fx plot.

--- das4whales/plot.py
+++ das4whales/plot.py
@@ -68,6 +68,7 @@ def plot_fx(trace, dist, fs, win_s=2, nfft=4096, fig_size=(12, 10), f_min=0, f_m
     # Run through the data
     for ind in range(nb_subplots):
         fx = get_fx(trace[:, int(ind * win_s * fs):int((ind + 1) * win_s * fs):1], nfft)
+        fx = fx - np.median(fx, axis=0)
 
         # Plot
         r = ind // cols
