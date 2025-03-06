0b26e7d933328684f697a302a9cfe29634d8d126
qgoestch <quentin.goestchel@ens-paris-saclay.fr>
2025-03-06 14:23:40 -0800

    Fixed import

--- src/das4whales/__init__.py
+++ src/das4whales/__init__.py
@@ -1,4 +1,4 @@
-from das4whales import data_handle, dsp, plot, dask_wrap, tools, detect, improcess, loc, map
+from das4whales import data_handle, dsp, plot, dask_wrap, tools, detect, improcess, loc, map, spatial
 __version__ = "0.1.0"
 __author__ = 'Léa Bouffaut, Quentin Goestchel'
 
