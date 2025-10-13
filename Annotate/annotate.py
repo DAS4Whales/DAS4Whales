bf754a439c0c70d01504140ea5e03d28bd731eb2
qgoestch <quentin.goestchel@ens-paris-saclay.fr>
2025-10-13 09:30:04 -0700

    Minor change

--- Annotate/annotate.py
+++ Annotate/annotate.py
@@ -31,12 +31,12 @@ hv.extension('bokeh')
 pn.extension()
 # -
 
-batch_number = 5
+batch_number = 1
 output_dir = os.path.join('denoised_data', f'Batch_{batch_number}')
 north_files = sorted(glob.glob(os.path.join("..", output_dir, "Denoised_SNR_North_*.nc")))
 south_files = sorted(glob.glob(os.path.join("..", output_dir, "Denoised_SNR_South_*.nc")))
 
-id = 5 # 0 to 11 
+id = 0 # 0 to 11 
 print(f"Loading {north_files[id]}\n        {south_files[id]}")
 
 # +
