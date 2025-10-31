5b1489e1bc7785551a1942a14402d906c29c3405
esnyder <esnyder@cornell.edu>
2025-10-31 13:20:47 -0400

    debug

--- src/das4whales/__init__.py
+++ src/das4whales/__init__.py
@@ -1,4 +1,4 @@
 from das4whales import data_handle, dsp, plot, dask_wrap, tools, detect, loc, map, spatial
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = 'Léa Bouffaut, Quentin Goestchel'
 
